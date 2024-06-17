b2b2b41afc8875562499d3039ecac62ef0759cd8
NishiyamaTomokazu <tomokazu.nishiyama@gmail.com>
2024-06-17 11:51:34 +0900

    変更

--- js/function_data_send_ipad.js
+++ js/function_data_send_ipad.js
@@ -1,3 +1,62 @@
+// //元のプログラム
+// //転送処理(iPad)
+// function sendHID_iPad() {    
+// 	try{
+// 	//プログラムの自動保存
+// 	autosave();
+		
+//     //文字プログラムに変換
+//     outputProData();
+//     //転送データに変換
+//     changeSendData();
+// 	//改行コードを\nに統一
+//     var text  = document.getElementById('sendText').value.replace(/\r\n|\r/g, "\n");
+//     var lines = text.split( '\n' );    
+//     if (lines.length > 120){ 
+// 		alert("データ転送量が超えています。");
+// 		return;
+// 	}
+//     for  (var i = 0; i < lines.length; i++) { 
+//         if (lines[i] == "230"){
+//             break;
+//         }
+//         if (i == lines.length - 1){
+//             alert("開始命令がありません。");
+//             throw new Error("エラーメッセージ");
+//         }
+//     }
+//     //for  (var i = 0; i < lines.length; i++) { 
+//     //    if (lines[i] == "231"){
+//     //        break;
+//     //    }
+//     //    if (i == lines.length - 1){
+//     //        alert("終了命令がありません。");
+//     //        throw new Error("エラーメッセージ");
+//     //    }
+//     //}
+        
+// 	document.form6.textarea5.value ="";
+//     var blcnt = Math.ceil(lines.length / 16);//32バイトずつ転送するので何ブロックあるか
+//     for  (var i = 0; i < blcnt; i++) {
+// 		var sendArray = new Array(19);   
+//     	sendArray.fill(0);
+// 		sendArray[0] = 253;
+// 		sendArray[1] = 1;// 1:転送 2:実行
+// 		sendArray[2] = i + 1;
+// 		for (var j = 0; j < 16; j++) {
+// 			if ((i * 16 + j) > lines.length -1){break;}
+//         	sendArray[j + 3] = Number(lines[i * 16 + j]);			
+// 		}
+// 		sendDataBySound(sendArray);  				
+//     	sleep(500);   		  
+// 		//console.log(sendArray);
+// 		document.form6.textarea5.value +=sendArray+"\n";
+// 	}
+
+		
+//     }catch(e){}
+// }
+
 //転送処理(iPad)
 function sendHID_iPad() {    
 	try{
@@ -57,10 +116,9 @@ function sendHID_iPad() {
         sendArray[1] = 1;
         sendArray[2] = 2;
         sendArray[3] = 3;
-        sendArray[4] = 4;
-        sendArray[5] = 5;   //1:転送　2:実行
-        sendArray[6] = 6;   //これ以降に実際のデータを送信する
-        sendArray[7] = 7;
+        sendArray[4] = 253;
+        sendArray[5] = 1;   //1:転送　2:実行
+        sendArray[6] = i + 1;   //これ以降に実際のデータを送信する
 		for (var j = 0; j < 16; j++) {
 			if ((i * 16 + j) > lines.length -1){break;}
         	sendArray[j + 7] = Number(lines[i * 16 + j]);			
@@ -78,36 +136,36 @@ function sendHID_iPad() {
 function runHID_iPad(){
 	var sendArray = new Array(23);   
 	sendArray.fill(0);
-	// sendArray[0] = 253;
-	// sendArray[1] = 2;// 1:転送 2:実行
-    // sendArray[0] = 0;
-    // sendArray[1] = 0;
-    // sendArray[2] = 0;
-    // sendArray[3] = 0;
-    // sendArray[4] = 253;
-    // sendArray[5] = 2;       //1:転送 2:実行
+	sendArray[0] = 253;
+	sendArray[1] = 2;// 1:転送 2:実行
+    sendArray[0] = 0;
+    sendArray[1] = 0;
+    sendArray[2] = 0;
+    sendArray[3] = 0;
+    sendArray[4] = 253;
+    sendArray[5] = 2;       //1:転送 2:実行
 
-    sendDataArray[0] = 0;
-    sendDataArray[1] = 1;
-    sendDataArray[2] = 2;
-    sendDataArray[3] = 3;
+    // sendDataArray[0] = 0;
+    // sendDataArray[1] = 1;
+    // sendDataArray[2] = 2;
+    // sendDataArray[3] = 3;
 
-    sendDataArray[4] = 4;
-    sendDataArray[5] = 5;
-    sendDataArray[6] = 6;
-    sendDataArray[7] = 7;
-    sendDataArray[8] = 8;
-    sendDataArray[9] = 9;
-    sendDataArray[10] = 10;
-    sendDataArray[11] = 11;
-    sendDataArray[12] = 12;
-    sendDataArray[13] = 13;
-    sendDataArray[14] = 14;
-    sendDataArray[15] = 15;
-    sendDataArray[16] = 16;
-    sendDataArray[17] = 17;
-    sendDataArray[18] = 18;
-    sendDataArray[19] = 19;
+    // sendDataArray[4] = 4;
+    // sendDataArray[5] = 5;
+    // sendDataArray[6] = 6;
+    // sendDataArray[7] = 7;
+    // sendDataArray[8] = 8;
+    // sendDataArray[9] = 9;
+    // sendDataArray[10] = 10;
+    // sendDataArray[11] = 11;
+    // sendDataArray[12] = 12;
+    // sendDataArray[13] = 13;
+    // sendDataArray[14] = 14;
+    // sendDataArray[15] = 15;
+    // sendDataArray[16] = 16;
+    // sendDataArray[17] = 17;
+    // sendDataArray[18] = 18;
+    // sendDataArray[19] = 19;
 	
 	sendDataBySound(sendArray);
 }
