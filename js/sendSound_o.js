d34d7051cdc7b4c56137a5b962a53e1f0f2dc7b0
NishiyamaTomokazu <tomokazu.nishiyama@gmail.com>
2024-06-17 10:33:05 +0900

    個数変えた

--- js/sendSound_o.js
+++ js/sendSound_o.js
@@ -61,8 +61,8 @@ var myArrayBuffer = audioCtx.createBuffer(2,frameCount,audioCtx.sampleRate);
  * 
 **************************************/
 //送信用のデータの入った配列
-//let sendDataArray = Array(19);
-let sendDataArray = Array(24);      //Bluetooth版のとき最初の4つ分が無視される
+let sendDataArray = Array(19);
+//let sendDataArray = Array(24);      //Bluetooth版のとき最初の4つ分が無視される
 
 /**************************************
  * データの転送方法
@@ -122,25 +122,25 @@ function connect_iPad(){
 
     sendDataArray[4] = 253;
     sendDataArray[5] = 5;
-    sendDataArray[6] = 6;
-    sendDataArray[7] = 7;
-    sendDataArray[8] = 8;
-    sendDataArray[9] = 9;
-    sendDataArray[10] = 10;
-    sendDataArray[11] = 11;
-    sendDataArray[12] = 12;
-    sendDataArray[13] = 13;
-    sendDataArray[14] = 14;
-    sendDataArray[15] = 15;
-    sendDataArray[16] = 16;
-    sendDataArray[17] = 17;
-    sendDataArray[18] = 18;
-    sendDataArray[19] = 19;
-    sendDataArray[20] = 20;
-    sendDataArray[21] = 21;
-    sendDataArray[22] = 22;
-    sendDataArray[23] = 23;
-    sendDataArray[24] = 24;
+    // sendDataArray[6] = 6;
+    // sendDataArray[7] = 7;
+    // sendDataArray[8] = 8;
+    // sendDataArray[9] = 9;
+    // sendDataArray[10] = 10;
+    // sendDataArray[11] = 11;
+    // sendDataArray[12] = 12;
+    // sendDataArray[13] = 13;
+    // sendDataArray[14] = 14;
+    // sendDataArray[15] = 15;
+    // sendDataArray[16] = 16;
+    // sendDataArray[17] = 17;
+    // sendDataArray[18] = 18;
+    // sendDataArray[19] = 19;
+    // sendDataArray[20] = 20;
+    // sendDataArray[21] = 21;
+    // sendDataArray[22] = 22;
+    // sendDataArray[23] = 23;
+    // sendDataArray[24] = 24;
     sendDataBySound(sendDataArray);
 }
 
