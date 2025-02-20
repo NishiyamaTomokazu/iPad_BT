644b5efaa95e79374dfed676ac90a98533b6523e
NishiyamaTomokazu <tomokazu.nishiyama@gmail.com>
2025-02-20 16:54:10 +0900

    再生

--- js/sendSound_o.js
+++ js/sendSound_o.js
@@ -301,7 +301,7 @@ function outputSoundData(binaryDataArray) {
     });
     //無音を再生
     playSilentAudio();
-    console.log("無音再生");
+    console.log("無音★再生");
     //console.log(newArray);
     var source = audioCtx.createBufferSource();     //出力用のバッファを作成
     source.buffer = myArrayBuffer;                  //出力用のバッファに変換したデータを入れる
